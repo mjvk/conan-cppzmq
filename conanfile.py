e555a656b73f483f16aa642cce17f1dcf61bc850
Mark Jan van Kampen <mjvk@allseas.com>
2019-11-04 18:25:07 +0100

    Updates to cppzmq to 4.5.0 and zmq to 4.3.2

--- conanfile.py
+++ conanfile.py
@@ -4,7 +4,7 @@ from conans import ConanFile, CMake, tools
 
 class CppZmqConan(ConanFile):
     name = "cppzmq"
-    version = "4.4.1"
+    version = "4.5.0"
     url = "https://github.com/bincrafters/conan-cppzmq"
     homepage = "https://github.com/zeromq/cppzmq"
     description = "C++ binding for 0MQ"
@@ -15,14 +15,14 @@ class CppZmqConan(ConanFile):
     exports_sources = ["CMakeLists.txt"]
     generators = "cmake"
     no_copy_source = True
-    requires = "zmq/4.2.5@bincrafters/stable"
+    requires = "zmq/4.3.2@bincrafters/stable"
 
     @property
     def _source_subfolder(self):
         return "source_subfolder"
 
     def source(self):
-        sha256 = "117fc1ca24d98dbe1a60c072cde13be863d429134907797f8e03f654ce679385"
+        sha256 = "64eb4e58eaf0c77505391c6c9a606cffcb57c6086f3431567a1ef4a25b01fa36"
         tools.get("{0}/archive/v{1}.tar.gz".format(self.homepage, self.version), sha256=sha256)
         extracted_dir = self.name + "-" + self.version
         os.rename(extracted_dir, self._source_subfolder)
